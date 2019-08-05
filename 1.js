ec89934732d28caaf1242587c72820be5e883beb
freebrax1111 <53756833+freebrax1111@users.noreply.github.com>
2019-08-05 16:41:36 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+	$('.slider-inner, .news__slider-inner').slick({
+		nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		infinite: false
+	});
+
+
+
+
+	$('select').styler()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+//https://codepen.io/fancyapps/pen/zJgXyg?editors=1100
+  //https://icomoon.io/
+  //https://necolas.github.io/normalize.css/
+  //https://kenwheeler.github.io/slick/
+//http://dimox.name/jquery-form-styler/
+
+
